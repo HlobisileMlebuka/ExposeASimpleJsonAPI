d496986ab1ca8e693bee6a44ab961b701bcb37d5
HlobisileMlebuka <Mlebukah@gmail.com>
2019-08-28 17:47:46 +0200

    update feature to be checked

--- part_1_data_layer.py
+++ part_1_data_layer.py
@@ -1,13 +1,14 @@
 
 
 import json
-import os, re, glob
+import os, re, itertools
 MY_PATH = '/home/klobie/'
 
 class Visitor:
     
     def __init__(self, full_name, age, date_of_visit, time_of_visit, comments, visitor_assistant, id=None):
         
+        self.id = id
         self.full_name = full_name
         self.age = age 
         self.date_of_visit = date_of_visit
@@ -16,57 +17,69 @@ class Visitor:
         self.visitor_assistant = visitor_assistant
         
     def generate_id(self):
+        """generate an id, relative to the last recorded id"""       
 
         #search through a directory for all visitor objects
         files_in_directory = os.listdir(MY_PATH)
-        #find maximum id numbers in files
-        for files in files_in_directory:
-            visitor_id = re.findall(r"((?<=visitor_)\d(?=\.json))", files) 
-            last_id = max(visitor_id)
-            id = last_id + 1
+        visitor_id_list= []
+
+        #find list of id numbers in files_in_directory
+        for file_name in files_in_directory:
+            if re.findall(r"((?<=visitor_)\d(?=\.json))", file_name): 
+                pattern = re.findall(r"((?<=visitor_)\d(?=\.json))", file_name)
+                visitor_id_list.append(pattern)
         
-        return id 
+        #flatten visitor_id_list
+        visitor_id = list(itertools.chain.from_iterable(visitor_id_list))
         
+        #get the maximum number and add it to 1         
+        last_recorded_id = max(visitor_id, key=lambda x:int(x))
+        id = int(last_recorded_id) + 1 
+
+        return id
+                        
     def save(self):
-        """saves the visitor as a json file"""
 
-        visitor_dictionary = {'full_name':self.full_name, 'age':self.age, 'date_of_visit':self.date_of_visit, 'Time of visit':self.time_of_visit, 'comments':self.comments, 'visitor_assistant':self.visitor_assistant}
+        """"generates an id for the visiotor if there is no id and dumps the visitor as a json file"""
         
-        with open(f"{MY_PATH}/visitor_{id}.json", "w+") as dumped_data_file:
-            visitor_details = json.dump(visitor_dictionary, dumped_data_file)
-
-        return visitor_details
+        self.id = self.id or self.generate_id()
         
-
+        visitor_dictionary = {'full_name':self.full_name, 'age':self.age, 'date_of_visit':self.date_of_visit, 'time_of_visit':self.time_of_visit, 'comments':self.comments, 'visitor_assistant':self.visitor_assistant, 'id':self.id}
+        
+        with open(f"{MY_PATH}/visitor_{self.id}.json", "a+") as dumped_data_file:
+            json.dump(visitor_dictionary, dumped_data_file)
+        
+                
     def load(self, id):           
                 
         # search for json file in all directories in \"visitor_{some_number}.json\" format.
-        files = glob.glob(MY_PATH + f'/**/*{id}.json', recursive=True)
-        visitor_file = [x for x in files if f'visitor_{id}.json' in x]
+        # files_in_directory = os.listdir(MY_PATH) 
+        visitor_file = (f'{MY_PATH}/visitor_{self.id}.json')
                         
         #open visitor file and load as a python object
         with open(visitor_file, 'r') as loaded_data_file:
             visitor_details = json.load(loaded_data_file)
-
+            
         return Visitor(**visitor_details)
        
                 
 if __name__ == "__main__":
     
        
-    Alice = Visitor("Alice Smith", 12, "12/4/2019", "12:45", "forgot her shoes", "Maxwell, 1")
-    Alice.save()
-    Mike = Visitor('Mike Smith', 32, "12/04/2018", "12:47", "Alice's husband", "Maxwell", 2)
+    Samantha = Visitor("Samantha Smith", 12, "12/4/2019", "12:45", "this is the umpteeth try", "Maxwell",1)
+    
+    Samantha.save()
+    Mike = Visitor("Mike Smith", 32, "12/04/2018", "12:47", "Samantha's husband", "Maxwell")
     Mike.save()
-    Alice.age = 21
-    Alice.comments = 'hello nurse'
-    print(Alice.save())
-    Alice.age = 45
-    Alice.save()
 
-    Mike.load(2)
-    Mike.comments = "had a great time" 
-    Mike.save()
+    Samantha.age = 21
+    Samantha.comments = 'hello nurse'
+    Samantha.save()
+
+    # Mike.load(2)
+    # Mike.comments = "had a great time" 
+
+    # print(Mike.save())
     
     
     
@@ -100,5 +113,4 @@ if __name__ == "__main__":
 
         
             
-
 

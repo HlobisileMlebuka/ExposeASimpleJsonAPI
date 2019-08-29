e645cc4aa299f3775b3ce957892818ed489cb84c
HlobisileMlebuka <Mlebukah@gmail.com>
2019-08-29 14:48:33 +0200

    load method not working

--- part_1_data_layer.py
+++ part_1_data_layer.py
@@ -1,7 +1,8 @@
 
 
 import json
-import os, re, itertools
+import os, re
+import itertools 
 MY_PATH = '/home/klobie/'
 
 class Visitor:
@@ -15,6 +16,7 @@ class Visitor:
         self.time_of_visit = time_of_visit
         self.comments = comments
         self.visitor_assistant = visitor_assistant
+        
         
     def generate_id(self):
         """generate an id, relative to the last recorded id"""       
@@ -37,6 +39,20 @@ class Visitor:
         id = int(last_recorded_id) + 1 
 
         return id
+
+    def load(self,id):           
+        # self.id = id
+        # search for json file in all directories in \"visitor_{some_number}.json\" format.
+        # files_in_directory = os.listdir(MY_PATH) 
+        visitor_file = (f'{MY_PATH}/visitor_{id}.json')
+                        
+        #open visitor file and load as a python object
+        with open(visitor_file, 'r') as loaded_data_file:
+            visitor_details = json.load(loaded_data_file)
+
+        visitor = json.loads(visitor_details)
+        
+        return Visitor(**visitor)
                         
     def save(self):
 
@@ -45,41 +61,32 @@ class Visitor:
         self.id = self.id or self.generate_id()
         
         visitor_dictionary = {'full_name':self.full_name, 'age':self.age, 'date_of_visit':self.date_of_visit, 'time_of_visit':self.time_of_visit, 'comments':self.comments, 'visitor_assistant':self.visitor_assistant, 'id':self.id}
+        visitor = json.dumps(visitor_dictionary, indent=2)
         
-        with open(f"{MY_PATH}/visitor_{self.id}.json", "a+") as dumped_data_file:
-            json.dump(visitor_dictionary, dumped_data_file)
-        
-                
-    def load(self, id):           
-                
-        # search for json file in all directories in \"visitor_{some_number}.json\" format.
-        # files_in_directory = os.listdir(MY_PATH) 
-        visitor_file = (f'{MY_PATH}/visitor_{self.id}.json')
-                        
-        #open visitor file and load as a python object
-        with open(visitor_file, 'r') as loaded_data_file:
-            visitor_details = json.load(loaded_data_file)
+        with open(f"{MY_PATH}/visitor_{self.id}.json", "w+") as dumped_data_file:
+            json.dump(visitor, dumped_data_file)      
             
-        return Visitor(**visitor_details)
-       
                 
 if __name__ == "__main__":
     
        
-    Samantha = Visitor("Samantha Smith", 12, "12/4/2019", "12:45", "this is the umpteeth try", "Maxwell",1)
-    Samantha.save()
+    # Samantha = Visitor("Samantha Smith", 12, "12/4/2019", "12:45", "this is the umpteeth try", "Maxwell",1)
+    # Samantha.save()
     
-    Mike = Visitor("Mike Smith", 32, "12/04/2018", "12:47", "Samantha's husband", "Maxwell")
-    Mike.save()
+    Mike = Visitor("Mike Smith", 32, "12/04/2018", "12:47", "Samantha's husband", "Maxwell",2)
+    # Mike.save()
 
-    Samantha.age = 21
-    Samantha.comments = 'hello nurse'
-    Samantha.save()
-
+    # Samantha.age = 21
+    # Samantha.comments = 'hi'
+    # Samantha.save()
+    
+    #You had to initiate the class with positional arguments
+    
     Mike.load(2)
+
     Mike.comments = "had a great time" 
-    Mike.save()
-    
+    Mike.age = 22
+    Mike.save()   
     
     
 
